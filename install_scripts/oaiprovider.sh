06a092095e9972031d8c1a0f15a3ecf8748f543d
Cole Hudson <cole.hudson@wayne.edu>
2016-03-15 19:49:06 +0000

    adding bash install option

--- install_scripts/oaiprovider.sh
+++ install_scripts/oaiprovider.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 echo "---- Installing OAIProvider for Fedora Commons ------------------------------------------------"
 
 #### GET ENVARS #################################################
