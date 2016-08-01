9fd3b809041a4e384b531952dfc35d67cb347bbd
Graham Hukill <ghukill@gmail.com>
2016-08-01 14:26:41 -0400

    updating downloads and readux install

--- install_scripts/readux.sh
+++ install_scripts/readux.sh
@@ -15,10 +15,24 @@ else
 fi
 #################################################################
 
+# install ruby requirements
+apt-get -y install python-software-properties
+apt-add-repository -y ppa:brightbox/ruby-ng
+apt-get -y update
+apt-get -y install ruby2.2 ruby-switch
+ruby-switch --set ruby2.2
+apt-get -y install ruby2.2-dev
+
+
+# install teifacsimile_to_jekyll gem
+# https://github.com/emory-libraries-ecds/teifacsimile-to-jekyll
+gem install $SHARED_DIR/downloads/readux/teifacsimile_to_jekyll-0.6.0.gem
+
 # clone readux repository
 cd /opt
 git clone https://github.com/WSULib/readux.git
 cd readux
+git checkout wsu
 
 # use ouroboros venv
 workon ouroboros
@@ -28,15 +42,15 @@ pip install fabric
 fab build
 python manage.py syncdb
 
-
 # copy config, and update hosts
 cp $SHARED_DIR/downloads/readux/localsettings.py /opt/readux/readux/localsettings.py
 
-# install WSUDOR fork / copy of Emory's eultheme
+# install WSUDOR fork / copy of Emory's eultheme for local editing
 cd /opt
 git clone https://github.com/WSULib/wsudor_django_theme
 cd wsudor_django_theme
-python setup.py install
+# install from github
+pip install -e git://github.com/WSUlib/wsudor_django_theme.git#egg=eultheme
 
 # chown
 chown -R ouroboros:admin /opt/readux
