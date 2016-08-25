56f889d89c85e0a92723ead6710e1d9b5f51ef30
Cole Hudson <colehudson@gmail.com>
2016-08-25 12:27:45 -0400

    Updating locations of cron reference

--- install_scripts/utilities.sh
+++ install_scripts/utilities.sh
@@ -32,13 +32,13 @@ git clone https://github.com/WSULib/dc2Solr.git
 # Write cronjobs
 # crontab -l | { cat; echo "0 * * * * some_entry"; } | crontab -
 crontab -l | { cat; echo "# Downloads item records from DC, indexes in Solr"; } | crontab -
-crontab -l | { cat; echo "#0 23    * * *      cd /var/opt/solr_utilities/dc2Solr && python dc2Solr.py all >/dev/null 2>&1"; } | crontab -
+crontab -l | { cat; echo "#0 23    * * *      cd /opt/utilities/dc2Solr && /usr/local/lib/venvs/global/bin/python dc2Solr.py all >/dev/null 2>&1"; } | crontab -
 crontab -l | { cat; echo "# Runs utility to harvest BioMed Central SWORD deposits in Fedora for ingest to Digital Commons"; } | crontab -
-crontab -l | { cat; echo "#0 0    * * 1      python /var/opt/fedora_utilities/SWORD2DC/SWORD2DC.py >/dev/null 2>&1"; } | crontab -
+crontab -l | { cat; echo "#0 0    * * 1      /usr/local/lib/venvs/global/bin/python /opt/utilities/SWORD2DC/SWORD2DC.py >/dev/null 2>&1"; } | crontab -
 crontab -l | { cat; echo "# Emails authors for DigitalCommons@WayneState's Paper of the Day"; } | crontab -
-crontab -l | { cat; echo "#0 12    * * *      cd /var/opt/digitalcommons_utilities/POTDemailer && python POTDemailer.py >/dev/null 2>&1"; } | crontab -
+crontab -l | { cat; echo "#0 12    * * *      cd /opt/utilities/POTDemailer && /usr/local/lib/venvs/global/bin/python POTDemailer.py >/dev/null 2>&1"; } | crontab -
 crontab -l | { cat; echo "# Runs script to update sitemaps for Google crawler"; } | crontab -
-crontab -l | { cat; echo "#0 0    * * 0      cd /var/opt/solr_utilities/digital-collections-sitemaps && python digital-collections-sitemaps.py >/dev/null 2>&1"; } | crontab -
+crontab -l | { cat; echo "#0 0    * * 0      cd /opt/utilities/digital-collections-sitemaps && /usr/local/lib/venvs/global/bin/python digital-collections-sitemaps.py >/dev/null 2>&1"; } | crontab -
 
 # Install Dependencies
 sudo pip install pyPdf
