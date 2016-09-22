36e39e296ccbdddf4388ffda7ee322aa44b0774c
Graham Hukill <ghukill@gmail.com>
2016-09-22 08:46:37 -0400

    updates for readux build

--- install_scripts/readux.sh
+++ install_scripts/readux.sh
@@ -23,7 +23,6 @@ apt-get -y install ruby2.2 ruby-switch
 ruby-switch --set ruby2.2
 apt-get -y install ruby2.2-dev
 
-
 # install teifacsimile_to_jekyll gem
 # https://github.com/emory-libraries-ecds/teifacsimile-to-jekyll
 gem install $SHARED_DIR/downloads/readux/teifacsimile_to_jekyll-0.6.0.gem
@@ -32,7 +31,7 @@ gem install $SHARED_DIR/downloads/readux/teifacsimile_to_jekyll-0.6.0.gem
 cd /opt
 git clone https://github.com/WSULib/readux.git
 cd readux
-git checkout wsu
+git checkout wsu_deploy
 
 # use ouroboros venv
 workon ouroboros
@@ -40,22 +39,36 @@ workon ouroboros
 # from deployment notes: http://readux.readthedocs.io/en/develop/deploynotes.html
 pip install fabric
 fab build
-python manage.py syncdb
 
-# copy config
+# copy config and replace values
 cp $SHARED_DIR/downloads/readux/localsettings.py /opt/readux/readux/localsettings.py
 cp $SHARED_DIR/downloads/readux/settings.py /opt/readux/readux/settings.py
+# replace these
+# FEDORA_USER, FEDORA_ADMIN_PASSWORD, VM_HOST
+sed -i "s/FEDORA_ADMIN_USERNAME/$FEDORA_ADMIN_USERNAME/g" /opt/readux/readux/localsettings.py
+sed -i "s/FEDORA_ADMIN_PASSWORD/$FEDORA_ADMIN_PASSWORD/g" /opt/readux/readux/localsettings.py
+sed -i "s/VM_HOST/$VM_HOST/g" /opt/readux/readux/localsettings.py
+
+# update db
+python manage.py syncdb
+python manage.py migrate
 
 # install WSUDOR fork / copy of Emory's eultheme for local editing
 cd /opt
 git clone https://github.com/WSULib/wsudor_django_theme
 cd wsudor_django_theme
-# install from github
-pip install -e git://github.com/WSUlib/wsudor_django_theme.git#egg=eultheme
+pip install --upgrade .
+
+# collect static files
+cd /opt/readux
+python manage.py collectstatic --noinput
 
 # chown
 chown -R ouroboros:admin /opt/readux
 chown -R ouroboros:admin /opt/wsudor_django_theme
+
+# restart apache
+service apache2 restart
 
 # close
 deactivate
